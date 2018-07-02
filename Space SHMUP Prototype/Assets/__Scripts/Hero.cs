f9ee4f2f5fff8d4f86f323cca0a1357fd823412c
Un <keepintine@gmail.com>
2018-07-02 18:27:11 +0300

    Add interactive with PowerUps

--- Space SHMUP Prototype/Assets/__Scripts/Hero.cs
+++ Space SHMUP Prototype/Assets/__Scripts/Hero.cs
@@ -16,6 +16,8 @@ public class Hero : MonoBehaviour {
     [SerializeField]
     private float _shieldLevel = 1;
 
+    public Weapon[] weapons;
+
     public bool ____________________;
 
     public Bounds bounds;
@@ -28,6 +30,10 @@ public class Hero : MonoBehaviour {
     void Awake() {
         S = this;
         bounds = Utils.CombineBoundsOfChildren(this.gameObject);
+
+        // Возвращает оружие чтобы герой начал с 1 бластером
+        ClearWeapons();
+        weapons[0].SetType(WeaponType.blaster);
     }
 
     // Update is called once per frame
@@ -75,11 +81,52 @@ public class Hero : MonoBehaviour {
                 shieldLevel--;
                 // Уничтожаем врага
                 Destroy(go);
+            } else if (go.tag == "PowerUp"){
+                AbsorbPowerUp(go);
             } else {
                 print("Triggered: " + go.name);
             }
         } else {
             print("Triggered: " + other.gameObject.name);
+        }
+    }
+
+    public void AbsorbPowerUp(GameObject go) {
+        PowerUp pu = go.GetComponent<PowerUp>();
+        switch (pu.type) {
+            case WeaponType.shield:
+                shieldLevel++;
+                break;
+            default:
+                // Это какой то из типо оружия
+                if (pu.type == weapons[0].type) {
+                    // Увеличиваем количество оружий этого типа
+                    Weapon w = GetEmptyWeaponSlot();    // Находим доступный слот
+                    if (w != null) {
+                        w.SetType(pu.type);
+                    }
+                } else {
+                    // Если это другое оружие
+                    ClearWeapons();
+                    weapons[0].SetType(pu.type);
+                }
+                break;
+        }
+        pu.AbsorbedBy(this.gameObject);
+    }
+
+    Weapon GetEmptyWeaponSlot() {
+        for (int i = 0; i < weapons.Length; i++) {
+            if(weapons[i].type == WeaponType.none) {
+                return (weapons[i]);
+            }
+        }
+        return (null);
+    }
+
+    void ClearWeapons() {
+        foreach(Weapon w in weapons) {
+            w.SetType(WeaponType.none);
         }
     }
 
